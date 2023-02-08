d63ed5d050e306ceb2c2ecfd6268bb3d13f325e7
zach2017 <myemail>
2023-02-08 12:00:27 -0600

    actuator working

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ repositories {
 }
 
 dependencies {
+	//implementation(platform("org.springframework.cloud:spring-cloud-dependencies"))
+	//annotationProcessor("org.springframework.cloud:spring-cloud-dependencies")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
